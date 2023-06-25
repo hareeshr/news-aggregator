e63f71721383ba1d1ffc614e8dedd6435821e1c4
Hareesh Ramachandran <hareeshr.2007@gmail.com>
2023-06-25 14:08:18 +0400

    docker

--- news_aggregator_backend/entrypoint.sh
+++ news_aggregator_backend/entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# set env variables
+# echo "NEWSAPI_API_KEY=${NEWSAPI_API_KEY}" >> /app/backend/.env
+# echo "NYTIMES_API_KEY=${NYTIMES_API_KEY}" >> /app/backend/.env
+# echo "GUARDIAN_API_KEY=${GUARDIAN_API_KEY}" >> /app/backend/.env
+
 # php artisan migrate
 php artisan key:generate
+
+# Generate application key
+php artisan key:generate
+
+# Update the .env file with MySQL environment variables
+# php -r "file_put_contents('.env', str_replace(['DB_DATABASE=laravel', 'DB_USERNAME=root', 'DB_PASSWORD='], ['DB_DATABASE=${MYSQL_DATABASE}', 'DB_USERNAME=${MYSQL_DATABASE}', 'DB_PASSWORD=${MYSQL_DATABASE}'], file_get_contents('.env')));"
+
 php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
