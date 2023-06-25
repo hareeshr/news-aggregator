5cc45d4d2ee8c4230a73e6fd07d6ff83e9958d62
Hareesh Ramachandran <hareeshr.2007@gmail.com>
2023-06-25 15:50:58 +0400

    docker files

--- news_aggregator_backend/entrypoint.sh
+++ news_aggregator_backend/entrypoint.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
 # set env variables
-# echo "NEWSAPI_API_KEY=${NEWSAPI_API_KEY}" >> /app/backend/.env
-# echo "NYTIMES_API_KEY=${NYTIMES_API_KEY}" >> /app/backend/.env
-# echo "GUARDIAN_API_KEY=${GUARDIAN_API_KEY}" >> /app/backend/.env
+echo "NEWSAPI_API_KEY=${NEWSAPI_API_KEY}" >> /app/backend/.env
+echo "NYTIMES_API_KEY=${NYTIMES_API_KEY}" >> /app/backend/.env
+echo "GUARDIAN_API_KEY=${GUARDIAN_API_KEY}" >> /app/backend/.env
 
-# php artisan migrate
-php artisan key:generate
 
 # Generate application key
 php artisan key:generate
 
 # Update the .env file with MySQL environment variables
-# php -r "file_put_contents('.env', str_replace(['DB_DATABASE=laravel', 'DB_USERNAME=root', 'DB_PASSWORD='], ['DB_DATABASE=${MYSQL_DATABASE}', 'DB_USERNAME=${MYSQL_DATABASE}', 'DB_PASSWORD=${MYSQL_DATABASE}'], file_get_contents('.env')));"
+php -r "file_put_contents('.env', str_replace(['DB_DATABASE=', 'DB_USERNAME=', 'DB_PASSWORD='], ['DB_DATABASE=${MYSQL_DATABASE}', 'DB_USERNAME=${MYSQL_USER}', 'DB_PASSWORD=${MYSQL_PASSWORD}'], file_get_contents('.env')));"
 
-php artisan cache:clear
-php artisan config:clear
+cat .env
+
+php artisan route:cache
 php artisan route:clear
+php artisan config:clear
+php artisan cache:clear
+php artisan optimize
+
+
+php artisan migrate --force
 
 php artisan serve --host=0.0.0.0
 exec docker-php-entrypoint "$@"
